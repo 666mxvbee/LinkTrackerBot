207a094bda3f18a0f5a08f3491457fca12646a48
666mxvbee <mitrasher.negativ@gmail.com>
2026-05-27 16:36:40 +0300

    feat(scrapper): wire database repositories

--- src/LinkTracker.Scrapper/Program.cs
+++ src/LinkTracker.Scrapper/Program.cs
@@ -4,7 +4,10 @@ using LinkTracker.Scrapper.Clients;
 using LinkTracker.Scrapper.Jobs;
 using LinkTracker.Scrapper.Configuration;
 using LinkTracker.Scrapper.Database;
-using Microsoft.EntityFrameworkCore.Migrations;
+using LinkTracker.Scrapper.Repositories.Sql;
+using LinkTracker.Scrapper.Repositories.Orm;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +17,25 @@ builder.Services.AddSwaggerGen();
 builder.Services.Configure<DatabaseOptions>(
     builder.Configuration.GetSection(DatabaseOptions.SectionName));
 
-builder.Services.AddSingleton<ILinkRepository, InMemoryLinkRepository>();
+var databaseOptions = builder.Configuration
+    .GetSection(DatabaseOptions.SectionName)
+    .Get<DatabaseOptions>() ?? new DatabaseOptions();
+
+builder.Services.AddSingleton(_ => NpgsqlDataSource.Create(databaseOptions.ConnectionString));
+
+builder.Services.AddDbContext<LinkTrackerDbContext>(options =>
+    options.UseNpgsql(databaseOptions.ConnectionString));
+
+if (databaseOptions.AccessType.Equals("ORM", StringComparison.OrdinalIgnoreCase))
+{
+    builder.Services.AddScoped<ILinkRepository, OrmLinkRepository>();
+    builder.Services.AddScoped<ITagRepository, OrmTagRepository>();
+}
+else
+{
+    builder.Services.AddScoped<ILinkRepository, SqlLinkRepository>();
+    builder.Services.AddScoped<ITagRepository, SqlTagRepository>();
+}
 
 builder.Services.AddHttpClient<GitHubClient>();
 builder.Services.AddHttpClient<StackOverflowClient>();
